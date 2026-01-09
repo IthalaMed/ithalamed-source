69e9fefb75b55fd95d322f6bfc22475dcfd178ab
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 12:36:43 +0200

    Add NotificationChannel enum for delivery channels

---
+++ ithalamed/packages/common/src/enums/notification-channel.enum.ts
@@ -0,0 +1,19 @@
+/**
+ * Notification delivery channels
+ */
+export enum NotificationChannel {
+  /** Push notification */
+  PUSH = 'push',
+  
+  /** SMS */
+  SMS = 'sms',
+  
+  /** Email */
+  EMAIL = 'email',
+  
+  /** WhatsApp */
+  WHATSAPP = 'whatsapp',
+  
+  /** In-app notification */
+  IN_APP = 'in_app',
+}
