eb8ef0d1c3b6035326e85ecc2904ea7552b7d6f2
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 17:50:43 +0200

    Export shared modules from index.ts

---
+++ ithalamed/services/auth-service/src/shared/index.ts
@@ -0,0 +1,6 @@
+export * from './decorators';
+export * from './filters/http-exception.filter';
+export * from './interceptors/transform.interceptor';
+export * from './interceptors/logging.interceptor';
+export * from './guards/jwt-auth.guard';
+export * from './guards/roles.guard';
