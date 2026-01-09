9d3c647fb582d99adb621e9cff7a18bab2ff9f53
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 17:17:54 +0200

    Create index.ts to export patient entities

---
+++ ithalamed/packages/database/src/entities/patient/index.ts
@@ -0,0 +1,7 @@
+// Export all patient entities
+export * from './patient. entity';
+export * from './patient-allergy.entity';
+export * from './patient-condition.entity';
+export * from './patient-emergency-contact.entity';
+export * from './patient-consent.entity';
+export * from './patient-document.entity';
