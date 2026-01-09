673a778506391cd9a53b60fb7c2e4b29789bcd2e
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 16:27:23 +0200

    Create index.ts for common package exports
    This file serves as an entry point for exporting shared resources.

---
+++ ithalamed/packages/common/src/index.ts
@@ -0,0 +1,24 @@
+/**
+ * @ithalamed/common
+ * 
+ * Shared types, enums, utilities, and constants for the IthalaMed platform. 
+ * This package is used across all services and applications.
+ */
+
+// Export all enums
+export * from './enums';
+
+// Export all types
+export * from './types';
+
+// Export all interfaces
+export * from './interfaces';
+
+// Export all utilities
+export * from './utils';
+
+// Export all errors
+export * from './errors';
+
+// Export all constants
+export * from './constants';
