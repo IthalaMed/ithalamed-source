4ccc72f411d0a9298b75c3e0c38b57aae5ea1ef8
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 18:36:18 +0200

    Fix exports and organize type declarations

--- ithalamed/packages/common/src/types/index.ts
+++ ithalamed/packages/common/src/types/index.ts
@@ -1,10 +1,17 @@
-// Export all types
+// Core types
 export * from './address.type';
-export * from './medical-aid. type';
+export * from './medical-aid.type';
 export * from './emergency-contact.type';
-export * from './vital-signs.type';
-export * from './device-info.type';
 export * from './pagination.type';
 export * from './api-response.type';
-export * from './audit-log.type';
-export * from './geo-location.type';
+export * from './device-info.type';
+export * from './jwt-payload.type';
+
+// Clinical types
+export * from './vital-signs.type';
+export * from './clinical. type';
+export * from './growth-chart.type';
+
+// Feature-specific types
+export * from './telemedicine.type';
+export * from './device-integration.type';
