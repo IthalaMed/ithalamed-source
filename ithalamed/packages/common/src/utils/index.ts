0355a8da284573c00c577649a55b622e9671b946
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 15:10:21 +0200

    Create index.ts to export utility functions

---
+++ ithalamed/packages/common/src/utils/index.ts
@@ -0,0 +1,7 @@
+// Export all utilities
+export * from './date. utils';
+export * from './string.utils';
+export * from './validation.utils';
+export * from './sa-id.utils';
+export * from './phone.utils';
+export * from './crypto.utils';
