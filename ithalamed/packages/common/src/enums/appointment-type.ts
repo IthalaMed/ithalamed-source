011cdab0c92a0656f727ab75d882f9e09ba934d7
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 17:59:18 +0200

    Refactor AppointmentType enum with new values
    Updated appointment types to include new categories and removed some existing ones.

--- ithalamed/packages/common/src/enums/appointment-type.ts
+++ ithalamed/packages/common/src/enums/appointment-type.ts
@@ -2,33 +2,32 @@
  * Types of appointments
  */
 export enum AppointmentType {
-  /** In-person consultation */
-  IN_PERSON = 'in_person',
-  
-  /** Video telemedicine consultation */
-  TELEMEDICINE_VIDEO = 'telemedicine_video',
-  
-  /** Audio-only telemedicine consultation */
-  TELEMEDICINE_AUDIO = 'telemedicine_audio',
-  
-  /** Home visit */
-  HOME_VISIT = 'home_visit',
-  
-  /** Follow-up appointment */
+  /** Standard consultation */
+  CONSULTATION = 'consultation',
+  /** Follow-up visit */
   FOLLOW_UP = 'follow_up',
-  
-  /** Routine checkup */
-  ROUTINE_CHECKUP = 'routine_checkup',
-  
-  /** Emergency consultation */
-  EMERGENCY = 'emergency',
-  
-  /** Procedure/Surgery */
+  /** Medical procedure */
   PROCEDURE = 'procedure',
-  
   /** Laboratory visit */
-  LAB_VISIT = 'lab_visit',
-  
+  LAB = 'lab',
   /** Imaging/Radiology */
   IMAGING = 'imaging',
+  /** Vaccination */
+  VACCINATION = 'vaccination',
+  /** Health screening */
+  SCREENING = 'screening',
+  /** Pre-operative assessment */
+  PRE_OPERATIVE = 'pre_operative',
+  /** Post-operative check */
+  POST_OPERATIVE = 'post_operative',
+  /** Annual physical/checkup */
+  ANNUAL_CHECKUP = 'annual_checkup',
+  /** Telemedicine consultation */
+  TELEMEDICINE = 'telemedicine',
+  /** Home visit */
+  HOME_VISIT = 'home_visit',
+  /** Emergency (walk-in) */
+  EMERGENCY = 'emergency',
+  /** Second opinion */
+  SECOND_OPINION = 'second_opinion',
 }
