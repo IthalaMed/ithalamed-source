fd74a5727eaa3dff2b4cbf637806841f494d0f99
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 15:49:41 +0200

    Add error index file to export error classes

---
+++ ithalamed/packages/common/src/errors/index.ts
@@ -0,0 +1,10 @@
+// Export all error classes
+export * from './base. error';
+export * from './validation.error';
+export * from './not-found.error';
+export * from './unauthorized.error';
+export * from './forbidden.error';
+export * from './conflict.error';
+export * from './bad-request.error';
+export * from './internal. error';
+export * from './error-codes';
