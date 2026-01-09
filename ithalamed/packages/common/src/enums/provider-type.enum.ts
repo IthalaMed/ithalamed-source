7b0b21c7b21cc4e0a319e652044b9a6d50d074f8
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 12:22:52 +0200

    Add enums for ProviderType and MedicalSpecialty
    Defines enums for healthcare provider types and medical specialties.

---
+++ ithalamed/packages/common/src/enums/provider-type.enum.ts
@@ -0,0 +1,73 @@
+/**
+ * Types of healthcare providers
+ */
+export enum ProviderType {
+  /** Medical Doctor */
+  DOCTOR = 'doctor',
+  
+  /** Registered Nurse */
+  NURSE = 'nurse',
+  
+  /** Medical Specialist */
+  SPECIALIST = 'specialist',
+  
+  /** Dentist */
+  DENTIST = 'dentist',
+  
+  /** Physiotherapist */
+  PHYSIOTHERAPIST = 'physiotherapist',
+  
+  /** Psychologist */
+  PSYCHOLOGIST = 'psychologist',
+  
+  /** Optometrist */
+  OPTOMETRIST = 'optometrist',
+  
+  /** Radiologist */
+  RADIOLOGIST = 'radiologist',
+  
+  /** Pathologist */
+  PATHOLOGIST = 'pathologist',
+  
+  /** Midwife */
+  MIDWIFE = 'midwife',
+  
+  /** Paramedic */
+  PARAMEDIC = 'paramedic',
+}
+
+/**
+ * Medical specialties
+ */
+export enum MedicalSpecialty {
+  GENERAL_PRACTICE = 'general_practice',
+  INTERNAL_MEDICINE = 'internal_medicine',
+  CARDIOLOGY = 'cardiology',
+  DERMATOLOGY = 'dermatology',
+  ENDOCRINOLOGY = 'endocrinology',
+  GASTROENTEROLOGY = 'gastroenterology',
+  GERIATRICS = 'geriatrics',
+  HEMATOLOGY = 'hematology',
+  INFECTIOUS_DISEASE = 'infectious_disease',
+  NEPHROLOGY = 'nephrology',
+  NEUROLOGY = 'neurology',
+  ONCOLOGY = 'oncology',
+  PULMONOLOGY = 'pulmonology',
+  RHEUMATOLOGY = 'rheumatology',
+  ALLERGY_IMMUNOLOGY = 'allergy_immunology',
+  ANESTHESIOLOGY = 'anesthesiology',
+  EMERGENCY_MEDICINE = 'emergency_medicine',
+  FAMILY_MEDICINE = 'family_medicine',
+  OBSTETRICS_GYNECOLOGY = 'obstetrics_gynecology',
+  OPHTHALMOLOGY = 'ophthalmology',
+  ORTHOPEDICS = 'orthopedics',
+  OTOLARYNGOLOGY = 'otolaryngology',
+  PEDIATRICS = 'pediatrics',
+  PLASTIC_SURGERY = 'plastic_surgery',
+  PSYCHIATRY = 'psychiatry',
+  RADIOLOGY = 'radiology',
+  SURGERY_GENERAL = 'surgery_general',
+  UROLOGY = 'urology',
+  PATHOLOGY = 'pathology',
+  PHYSICAL_MEDICINE = 'physical_medicine',
+}
