4b15778e79717bd3a14b0394dc81014f7e33e85d
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 12:40:11 +0200

    Export all types from index.ts

---
+++ ithalamed/packages/common/src/types/index.ts
@@ -0,0 +1,10 @@
+// Export all types
+export * from './address.type';
+export * from './medical-aid. type';
+export * from './emergency-contact.type';
+export * from './vital-signs.type';
+export * from './device-info.type';
+export * from './pagination.type';
+export * from './api-response.type';
+export * from './audit-log.type';
+export * from './geo-location.type';
