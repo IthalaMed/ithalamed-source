36c0582a1706dbefb3754e3f8980e27758492f8c
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 19:32:18 +0200

    Refactor patient entity exports for consistency

--- ithalamed/packages/database/src/entities/patient/index.ts
+++ ithalamed/packages/database/src/entities/patient/index.ts
@@ -1,7 +1,8 @@
-// Export all patient entities
-export * from './patient. entity';
-export * from './patient-allergy.entity';
-export * from './patient-condition.entity';
-export * from './patient-emergency-contact.entity';
+export * from './patient.entity';
 export * from './patient-consent.entity';
 export * from './patient-document.entity';
+export * from './patient-guardian.entity';
+export * from './patient-medical-aid.entity';
+export * from './patient-emergency-contact.entity';
+export * from './patient-allergy.entity';
+export * from './patient-condition.entity';
