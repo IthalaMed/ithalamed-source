da37b18a1a1dfce940aabfeba7d03be07f3d4583
seth-ithalamed <seth.bbd@gmail.com>
2026-01-09 15:44:20 +0200

    Create index.ts

---
+++ ithalamed/packages/common/src/interfaces/index.ts
@@ -0,0 +1,6 @@
+// Export all interfaces
+export * from './base-entity. interface';
+export * from './jwt-payload.interface';
+export * from './service-response.interface';
+export * from './event-payload.interface';
+export * from './repository.interface';
