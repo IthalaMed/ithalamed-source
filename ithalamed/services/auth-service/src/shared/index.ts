f86142b21d8d8c1df52b38c3c8d609d7d081e8ad
seth-ithalamed <seth.bbd@gmail.com>
2026-01-10 08:00:05 +0200

    Delete ithalamed/services/auth-service/src/shared directory

--- ithalamed/services/auth-service/src/shared/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './decorators';
-export * from './filters/http-exception.filter';
-export * from './interceptors/transform.interceptor';
-export * from './interceptors/logging.interceptor';
-export * from './guards/jwt-auth.guard';
-export * from './guards/roles.guard';
