86f00f3d3d012d75c2c72e1e4df04c5dccf6205e
seth-ithalamed <seth.bbd@gmail.com>
2026-01-10 08:16:49 +0200

    Create Public decorator for public endpoints
    Adds a decorator to mark endpoints as public, bypassing authentication.

---
+++ ithalamed/packages/common/src/nestjs/decorators/public.decorator.ts
@@ -0,0 +1,13 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IS_PUBLIC_KEY = 'isPublic';
+
+/**
+ * Decorator to mark an endpoint as public (no authentication required)
+ * 
+ * @example
+ * @Public()
+ * @Get('health')
+ * healthCheck() { ... }
+ */
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
