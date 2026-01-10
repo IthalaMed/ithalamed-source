90707392ce767845bdf2eecf7237526f0dd0e19f
seth-ithalamed <seth.bbd@gmail.com>
2026-01-10 07:10:06 +0200

    Create index.ts to export configuration modules

---
+++ ithalamed/services/auth-service/src/config/index.ts
@@ -0,0 +1,4 @@
+export { default as authConfig } from './auth.config';
+export { default as jwtConfig } from './jwt.config';
+export { default as databaseConfig } from './database.config';
+export { default as redisConfig } from './redis.config';
