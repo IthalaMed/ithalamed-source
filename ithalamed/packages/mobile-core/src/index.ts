bf661ccc3f59cf7c996c5b4cc71931665fafafd3
seth-ithalamed <seth.bbd@gmail.com>
2026-01-11 18:10:06 +0200

    Create index.ts to re-export shared modules

---
+++ ithalamed/packages/mobile-core/src/index.ts
@@ -0,0 +1,15 @@
+// ==================== RE-EXPORT ALL SHARED TYPES ====================
+// Mobile apps can import types from @ithalamed/mobile-core
+export * from '@ithalamed/shared-types';
+
+// ==================== CONFIG ====================
+export * from './config';
+
+// ==================== SERVICES ====================
+export * from './services';
+
+// ==================== STORE ====================
+export * from './store';
+
+// ==================== UTILS ====================
+export * from './utils';
