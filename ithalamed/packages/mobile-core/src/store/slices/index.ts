abef6b79cee48ea1428e381157f7079dbabce58e
seth-ithalamed <seth.bbd@gmail.com>
2026-01-11 18:50:23 +0200

    Create index.ts to export reducers from slices

---
+++ ithalamed/packages/mobile-core/src/store/slices/index.ts
@@ -0,0 +1,8 @@
+export { default as authReducer } from './authSlice';
+export * from './authSlice';
+
+export { default as patientReducer } from './patientSlice';
+export * from './patientSlice';
+
+export { default as uiReducer } from './uiSlice';
+export * from './uiSlice';
