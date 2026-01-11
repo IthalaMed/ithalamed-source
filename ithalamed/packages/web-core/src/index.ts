f51cd2b0323b17cf8ddab7a92805324dd6b61ca6
seth-ithalamed <seth.bbd@gmail.com>
2026-01-11 19:46:33 +0200

    Create index.ts to re-export shared modules

---
+++ ithalamed/packages/web-core/src/index.ts
@@ -0,0 +1,11 @@
+// Re-export all shared types
+export * from '@ithalamed/shared-types';
+
+// Config
+export * from './config';
+
+// Services
+export * from './services';
+
+// Utils
+export * from './utils';
