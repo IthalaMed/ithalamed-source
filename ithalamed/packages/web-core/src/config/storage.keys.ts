3ffd4833b0899f7257255c73b5d42cb521718d2c
seth-ithalamed <seth.bbd@gmail.com>
2026-01-11 19:34:42 +0200

    Add storage keys for application configuration

---
+++ ithalamed/packages/web-core/src/config/storage.keys.ts
@@ -0,0 +1,10 @@
+export const STORAGE_KEYS = {
+  ACCESS_TOKEN: 'ithalamed_access_token',
+  REFRESH_TOKEN: 'ithalamed_refresh_token',
+  USER_ID: 'ithalamed_user_id',
+  SESSION_ID: 'ithalamed_session_id',
+  USER_PREFERENCES: 'ithalamed_user_preferences',
+  LANGUAGE: 'ithalamed_language',
+  THEME: 'ithalamed_theme',
+  CACHED_USER:  'ithalamed_cached_user',
+};
